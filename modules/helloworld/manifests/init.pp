abe969514edf58ef995b15316b004765b9c1da82
josepalafox <Jose@puppetlabs.com>
2013-04-28 21:56:57 +0000

    initial commit helloworld

---
+++ modules/helloworld/manifests/init.pp
@@ -0,0 +1,15 @@
+# Class: helloworld
+#
+# This module manages helloworld
+#
+# Parameters: none
+#
+# Actions:
+#
+# Requires: see Modulefile
+#
+# Sample Usage:
+#
+class helloworld {
+  notify { helloworld: }
+}
